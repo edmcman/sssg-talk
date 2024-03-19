4117864cdc96012e542f3e522fb5353b7765d7fa
Edward J Schwartz <moo.github.z.edmcman@xoxy.net>
2024-03-19 14:24:57 -0400

    update to make more complicated decompilation

--- rect.cc
+++ rect.cc
@@ -1,17 +1,35 @@
 #include <stdio.h>
 
+typedef struct {
+  int length;
+  int width;
+  int area;
+  int perimeter;
+} rectangle;
+
+#define NRECT 10
+
+void calculateRectangle(rectangle *r) {
+    r->area = r->length * r->width;
+    r->perimeter = 2 * (r->length + r->width);
+}
+
 void calculateRectangle(int length, int width, int *area, int *perimeter) {
     *area = length * width;
     *perimeter = 2 * (length + width);
 }
 
 int main() {
-    int length, width, area, perimeter;
-    printf("Enter length and width: ");
-    scanf("%d %d", &length, &width);
+
+    rectangle rects[NRECT];
+
+    for (int i = 0; i < NRECT; i++) {
+        printf("Enter length and width: ");
+        scanf("%d %d", &rects[i].length, &rects[i].width);
     
-    calculateRectangle(length, width, &area, &perimeter);
-    printf("Area: %d, Perimeter: %d\n", area, perimeter);
+        calculateRectangle(&rects[i]);
+        printf("Area: %d, Perimeter: %d\n", rects[i].area, rects[i].perimeter);
+    }
     return 0;
 }
 
