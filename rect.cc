6e216924b636202605641188322f4b963c8999cb
Edward J Schwartz <moo.github.z.edmcman@xoxy.net>
2024-03-25 15:06:41 -0400

    fix src

--- rect.cc
+++ rect.cc
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define NRECT 5
+
 typedef struct {
   int length; int width; int area;  int perimeter;
 } rectangle;
+
 void calculateRectangle(rectangle *r) {
     r->area = r->length * r->width;
     r->perimeter = 2 * (r->length + r->width);
 }
+
 int main() {
     rectangle rects[NRECT];
     for (int i = 0; i < NRECT; i++) {
